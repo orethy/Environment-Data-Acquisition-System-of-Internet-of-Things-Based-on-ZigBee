b17e9bb0cdee6f098f0de1c030365f26e128ae8e
Dorethy_Alice <646846424@qq.com>
2023-02-06 21:04:09 +0800

    git push

--- show.py
+++ show.py
@@ -1,47 +1,47 @@
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 from tkinter import *
 import matplotlib
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 import urllib.request
 import json
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 
 
-def get_data(url,date):
+def get_data(url, date):
     reponse = urllib.request.urlopen(url)
     html = reponse.read().decode('utf-8')
-    PullData= json.loads(html)
+    PullData = json.loads(html)
     DataArry = PullData['data']
-    xar,yar=[],[]
+    xar, yar = [], []
     for each in DataArry:
 
-        if len(each)>1:
-            y,x=each
-            x=datetime.strptime(x,'%Y-%m-%d %H:%M:%S')
-            if x>date and x<(date+timedelta(days=1)):
+        if len(each) > 1:
+            y, x = each
+            x = datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
+            if x > date and x < (date + timedelta(days=1)):
                 xar.append(x)
                 yar.append(float(y))
 
-    return xar,yar
+    return xar, yar
 
 
 def drawPic():
-    url=var.get()
+    url = var.get()
     try:
-        sampleCount=datetime.strptime(inputEntry.get(),'%Y-%m-%d')
+        sampleCount = datetime.strptime(inputEntry.get(), '%Y-%m-%d')
 
     except:
-        sampleCount='2017-6-12'
-        print ('输入正确日期格式')
-        inputEntry.delete(0,END)
-        inputEntry.insert(0,'2017-6-12')
+        sampleCount = '2017-6-12'
+        print('输入正确日期格式')
+        inputEntry.delete(0, END)
+        inputEntry.insert(0, '2017-6-12')
     drawPic.f.clf()
-    drawPic.a=drawPic.f.add_subplot(111)
+    drawPic.a = drawPic.f.add_subplot(111)
 
-    x,y=get_data(url,sampleCount)
+    x, y = get_data(url, sampleCount)
     drawPic.a.set_xlim(min(x), max(x))
-    drawPic.a.plot(x,y,color='r',label='wendu',)
+    drawPic.a.plot(x, y, color='r', label='wendu', )
     xlabels = drawPic.a.get_xticklabels()
     for label in drawPic.a.xaxis.get_ticklabels():
         label.set_fontsize(5)
@@ -60,13 +60,13 @@ if __name__ == '__main__':
     wendu_url = 'http://localhost:8080/findAllWendu'
     shidu_url = 'http://localhost:8080/findAllShidu'
     gas_url = 'http://localhost:8080/findGas'
-    sun_url='http://localhost:8080/findGuangzhao'
-    data = [('温度', wendu_url), ('湿度', shidu_url), ('可燃气', gas_url),('光照',sun_url)]
+    sun_url = 'http://localhost:8080/findGuangzhao'
+    data = [('温度', wendu_url), ('湿度', shidu_url), ('可燃气', gas_url), ('光照', sun_url)]
 
     matplotlib.use('TkAgg')
 
-    root=Tk()
-    drawPic.f = Figure(figsize=(9,7), dpi=100)
+    root = Tk()
+    drawPic.f = Figure(figsize=(9, 7), dpi=100)
 
     drawPic.canvas = FigureCanvasTkAgg(drawPic.f, master=root)
 
@@ -80,15 +80,14 @@ if __name__ == '__main__':
         Radiobutton(root, text=each[0], variable=var, value=each[1]).grid(row=1, column=count, sticky=W)
         count += 1
 
-    Label(root,text='请输入查询的日期(Y-m-d)：').grid(row=2,column=0)
+    Label(root, text='请输入查询的日期(Y-m-d)：').grid(row=2, column=0)
 
-    inputEntry=Entry(root)
+    inputEntry = Entry(root)
 
-    inputEntry.grid(row=2,column=1)
+    inputEntry.grid(row=2, column=1)
 
-    inputEntry.insert(0,'2017-6-12')
+    inputEntry.insert(0, '2017-6-12')
 
-    Button(root,text='画图',command=drawPic).grid(row=2,column=2,columnspan=3)
-
+    Button(root, text='画图', command=drawPic).grid(row=2, column=2, columnspan=3)
 
     root.mainloop()
